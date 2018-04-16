5bf61d4b9a4746b5b33d3a6a2b3e589876290024
bluedot94 <36063219+bluedot94@users.noreply.github.com>
2018-04-16 12:48:43 +0900

    TEST

---
+++ test/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'Happy scenario' :client => {
+      client
+        .url('http://localhost:8080')
+        .waitForElementVisible('#app',1000)
+        .assert.containsText('h2','Welcome to')
+        .assert.hidden('p')
+        .click('button')
+        .waitForElementVisible('p',1000)
+        .assert.containsText('p','NightWatch')
+        .end();
+  }
+}
